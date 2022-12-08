96c99df82b3f593960a21a4ea8a65de55bc08a40
Mustakim-final <106617239+Mustakim-final@users.noreply.github.com>
2022-12-08 23:31:17 +0600

    some edit

--- app/src/main/java/com/example/messenger/MessageActivity.java
+++ app/src/main/java/com/example/messenger/MessageActivity.java
@@ -7,8 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DownloadManager;
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -415,8 +417,21 @@ public class MessageActivity extends AppCompatActivity {
                             String key=selectedItem.getKey();
                             String url=selectedItem.getImagePost();
 
+                            DownloadManager.Request request=new DownloadManager.Request(Uri.parse(url));
+                            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI |
+                                    DownloadManager.Request.NETWORK_MOBILE);
+                            request.setTitle("Download");
+                            request.setDescription("Downloading file...");
+                            request.allowScanningByMediaScanner();
+                            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,""+System.currentTimeMillis());
 
-                            downloadImage(url,position);
+
+                            DownloadManager manager= (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                            manager.enqueue(request);
+
+
+                            //downloadImage(url,position);
                         }
                     });
                 }
@@ -489,6 +504,7 @@ public class MessageActivity extends AppCompatActivity {
                     @Override
                     public void onError(Error error) {
                         Toast.makeText(MessageActivity.this,"Not Done",Toast.LENGTH_SHORT).show();
+                        pd.dismiss();
                     }
                 });
     }
